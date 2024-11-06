ccdbd972b2661df2d493f2757058ef742f98c152
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2024-11-06 18:14:11 +0000

    perguntas js

--- perguntas.js
+++ perguntas.js
@@ -13,3 +13,8 @@ criaCartao(
     'O que é uma função',
     'Uma função é um bloco de código que executa uma tarefa'
 )
+criaCartao(
+    'Filosifa',
+    'O que é o Renascimento?',
+    'Período culturam da história europeia, retomanda as influências da Antiguidade para contrapor à produção cultural a Idade Média.'
+)
